1945d1df25d04297cf75e7962d6e3c92fc2bd28d
vincent.ding <vincent.ding@rccchina.com>
2021-01-18 17:34:54 +0800

    templates+static+user login

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import HttpResponse
 
 # Create your views here.
 from .models import BlogArticles
@@ -7,6 +8,8 @@ from .models import BlogArticles
 def blog_title(request):
     blogs = BlogArticles.objects.all()
     return render(request, "blog/titles.html", {"blogs": blogs})
+    # return render(request, "blog/titles.html")
+    # return HttpResponse(request, "呵呵")
 
 
 def blog_article(request, article_id):
